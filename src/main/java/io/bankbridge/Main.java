2323e489abc886ba72a6ae0837b8bc086d66dc23
grygorii.mazur <grigoro.on.odessa@gmail.com>
2020-06-05 07:16:40 +0300

    init

--- src/main/java/io/bankbridge/Main.java
+++ src/main/java/io/bankbridge/Main.java
@@ -3,7 +3,6 @@ package io.bankbridge;
 import io.bankbridge.provider.LocalProvider;
 import io.bankbridge.provider.RemoteProvider;
 import io.bankbridge.handler.RequestHandler;
-import spark.Service;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
@@ -13,7 +12,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		
 		port(8080);
-		setupRemoteAPI();
 
 		RequestHandler remoteProvider = new RequestHandler(new RemoteProvider());
 		RequestHandler localProvider = new RequestHandler(new LocalProvider());
@@ -22,27 +20,4 @@ public class Main {
 		get("/v2/banks/all", remoteProvider::handle);
 	}
 
-	public static void setupRemoteAPI() {
-		Service remote = Service.ignite().port(1234).threadPool(10);
-
-		remote.get("/rbb", (request, response) ->{
-//					Thread.sleep(1000);
-					return "{\n" +
-							"\"bic\":\"1234\",\n" +
-							"\"countryCode\":\"GB\",\n" +
-							"\"auth\":\"OAUTH\"\n" +
-							"}";
-				}
-				);
-		remote.get("/cs", (request, response) -> "{\n" +
-				"\"bic\":\"5678\",\n" +
-				"\"countryCode\":\"CH\",\n" +
-				"\"auth\":\"OpenID\"\n" +
-				"}");
-		remote.get("/bes", (request, response) -> "{\n" +
-				"\"name\":\"Banco de espiritu santo\",\n" +
-				"\"countryCode\":\"PT\",\n" +
-				"\"auth\":\"SSL\"\n" +
-				"}");
-	}
 }
